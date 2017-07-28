c6c29cdeff788e0e5eae124e20275c91eec631df
karan1525 <karan1525@yahoo.co.in>
2017-07-28 01:01:05 -0700

    Hello world args tests

---
+++ VariationsHelloWorld/src/Main.kt
@@ -0,0 +1,14 @@
+fun main (args: Array<String>) {
+//    if (args.isEmpty()) {
+//        print("Please provide a name ")
+//        return
+//    }
+//
+//    println("Hello, $args!")
+    //${args}!
+
+    for (name in args) {
+        println("Hello, $name!")
+    }
+
+    }
