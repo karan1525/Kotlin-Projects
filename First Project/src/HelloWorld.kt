85f5e0f8bff8528a9e6f02a2e6ddcc1e28298b19
karan1525 <karan1525@yahoo.co.in>
2017-08-05 13:45:48 -0700

    Added val and var tests

--- First Project/src/HelloWorld.kt
+++ First Project/src/HelloWorld.kt
@@ -1,3 +1,11 @@
 fun main (args: Array <String> ) {
     println("Hello World!")
+
+    var name: String
+    name = "Hello Kotlin"
+
+    val test = "My Name is!"
+
+    println(name)
+    println(test)
 }
