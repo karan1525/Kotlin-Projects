96e1a03327942ceab37add478c84dcb249fddd74
karan1525 <karan1525@yahoo.co.in>
2017-08-06 20:18:53 -0700

    Lambda Expressions

--- Lambda/src/Lambda.kt
+++ Lambda/src/Lambda.kt
@@ -24,4 +24,29 @@ fun main (args: Array<String>) {
     println(sumAndPrint(5, 2))
     println(sumConcise(1, 2))
     println(sumEvenMoreConcise(2, 2))
+
+    val sum = { x: Int, y: Int -> x + y}
+    val action: () -> Unit = { println(42) }
+
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+
+    // When the lambda is the only argument to a function, you can also remove the empty parenthesis from the call
+    println(people.maxBy ({ p: Person -> p.age }))
+
+    //You can have the argument type inferred this way
+    println(people.maxBy { p -> p.age })
+
+    //Finally use it, if the type can be inferred
+    println(people.maxBy { it.age })
+
+    //To do non-local returns
+    fun lookForAlice(listOfPeople :List<Person>) {
+        listOfPeople.forEach label@ {
+            if (it.name == "Alice") return@label
+        }
+
+        println("Alice might be somewhere")
+    }
 }
+
+data class Person( val name: String, val age: Int)
