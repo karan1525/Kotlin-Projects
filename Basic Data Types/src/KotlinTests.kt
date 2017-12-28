06780e204fbb1b0509b53fed9321acca8efc0cd3
Karan Bhargava <karan1525@yahoo.co.in>
2017-12-27 22:08:41 -0800

    Ran lint inspection on project

--- Basic Data Types/src/KotlinTests.kt
+++ Basic Data Types/src/KotlinTests.kt
@@ -6,18 +6,18 @@ import java.util.*
 
 fun main(strings: Array<String>) {
     //Get the perimeter and area of a rectangle
-    val width:Double = 5.6
-    val height:Double = 8.5
+    val width = 5.6
+    val height = 8.5
     val perimeter  = 2.0 * (height + width)
 
-    val area = width * height;
+    val area = width * height
 
     System.out.printf("Perimeter is 2 * %.1f + %.1f = %.2f \n", height, width, perimeter)
     System.out.printf("Area is 2 * %.1f + %.1f = %.2f \n", height, width, area)
 
     //Converting minutes to years
     val minutesInYear: Double = (60 * 24 * 365).toDouble()
-    var input = Scanner(System.`in`)
+    val input = Scanner(System.`in`)
     print("Input the number of minutes: ")
 
     val min = input.nextDouble().toInt()
@@ -29,13 +29,13 @@ fun main(strings: Array<String>) {
 
     //Starts with code
 
-    val str1:String = "Red is my favorite color"
-    val str2:String = "Orange is also my favorite color"
+    val str1 = "Red is my favorite color"
+    val str2 = "Orange is also my favorite color"
 
-    val startStr:String = "Red"
+    val startStr = "Red"
 
-    val starts1:Boolean = str1.startsWith(startStr);
-    val starts2:Boolean = str2.startsWith(startStr);
+    val starts1:Boolean = str1.startsWith(startStr)
+    val starts2:Boolean = str2.startsWith(startStr)
 
     println("$str1 starts with $startStr ? $starts1") //returns true or false depending
     println("$str2 starts with $startStr ? $starts2") //prints boolean
@@ -75,8 +75,8 @@ fun printAllCaps(s: String?) {
 
 //Using the as operator for smart casting
 class Person(val firstName:String, val lastName: String) {
-    override fun equals(o: Any?): Boolean {
-        val otherPerson= o as? Person ?: return false
+    override fun equals(other: Any?): Boolean {
+        val otherPerson= other as? Person ?: return false
         return (otherPerson.firstName == firstName && otherPerson.lastName == lastName)
     }
 
