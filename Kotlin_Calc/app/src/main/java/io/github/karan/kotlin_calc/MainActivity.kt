6cf3e42de9b2c5a28ca8a7c73f19a82d8da70d06
Karan Bhargava <karan1525@yahoo.co.in>
2017-12-29 16:41:40 -0800

    Added multiplyButtonPressed method

--- Kotlin_Calc/app/src/main/java/io/github/karan/kotlin_calc/MainActivity.kt
+++ Kotlin_Calc/app/src/main/java/io/github/karan/kotlin_calc/MainActivity.kt
@@ -1,12 +1,22 @@
 package io.github.karan.kotlin_calc
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun multiplyButtonPressed(view: View) {
+        val operator = findViewById<View>(R.id.textViewOperator) as TextView
+        operator.setTextColor(Color.WHITE)
+        operator.setBackgroundColor(Color.BLUE)
+        operator.setText("*")
     }
 }
