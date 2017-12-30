bc5947785c9781973fd9bfa5c4e83857effb879a
Karan Bhargava <karan1525@yahoo.co.in>
2017-12-29 16:14:19 -0800

    Added changes using resources

--- KotlinResources/app/src/main/java/io/github/karanbhargava/kotlinresources/MainActivity.kt
+++ KotlinResources/app/src/main/java/io/github/karanbhargava/kotlinresources/MainActivity.kt
@@ -2,11 +2,14 @@ package io.github.karanbhargava.kotlinresources
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText.setText(getResources().getString(R.string.myButton2))
     }
 }
